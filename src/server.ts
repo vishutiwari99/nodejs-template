edd2d1452bfb2919fe351c7191fc3ede91819f26
Vaibhav Tiwari <vishutiwari99@gmail.com>
2025-03-15 13:10:53 +0530

    Add nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
+import app from "./app";
 import { Config } from "./config";
-
-function user() {
-  return Config.PORT;
-}
-
-user();
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    // eslint-disable-next-line no-console
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+startServer();
